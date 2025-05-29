f9fce772f39822ee19b7fb4a65c629a55099e016
Ramdani <dannymld41@gmail.com>
2025-05-29 10:06:25 +0700

    friend backend

--- sigma/friend/models.py
+++ sigma/friend/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
+
+class Friend(models.Model):
+    user = models.ForeignKey(User, related_name='friends', on_delete=models.CASCADE)
+    friend_username = models.ForeignKey(User, related_name='friend_of', on_delete=models.CASCADE)
+    accepted_at = models.DateTimeField("date accepted")
+
+    def __str__(self):
+        return self.friend_username.username
+
+
+
--- sigma/rootsite/settings.py
+++ sigma/rootsite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'profileinfo.apps.ProfileinfoConfig',
     'register.apps.RegisterConfig', 
+    'friend.apps.FriendConfig',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
